7d4ab644574e695a1a3f83474c6b1c8dc5145fbf
aneeq913 <47188268+aneeq913@users.noreply.github.com>
2022-01-19 20:28:22 +0000

    Added AppBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { Grid, Paper } from '@mui/material';
 import React from 'react';
 import './App.css';
+import Header from './app/features/Header/Header';
+import { styled } from '@mui/material/styles';
 
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   return (
     <div>
-      <p>Test</p>
+      <Header />
+
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+      </Grid>
     </div>
   );
 }
